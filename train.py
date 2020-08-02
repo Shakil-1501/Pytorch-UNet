89646d842e355b7132bbf0c849be3f32d7cb7b73
Shakil-1501 <63920152+Shakil-1501@users.noreply.github.com>
2020-08-02 19:53:09 +0530

    Update train.py

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ from torch.utils.tensorboard import SummaryWriter
 from utils.dataset import BasicDataset
 from torch.utils.data import DataLoader, random_split
 
-dir_img = 'data/imgs/'
+dir_img = 'data/images/'
 dir_mask = 'data/masks/'
 dir_checkpoint = 'checkpoints/'
 
